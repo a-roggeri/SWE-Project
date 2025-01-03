5c9a5a812cfcf0a1c3318864f1d7844b82d9b87d
a-roggeri <andrearoggeri22@gmail.com>
2025-01-03 16:03:36 +0100

    Creazione progetto Maven

---
+++ GestioneAppuntamenti/src/main/java/Tokyogroup/GestioneAppuntamenti/App.java
@@ -0,0 +1,13 @@
+package Tokyogroup.GestioneAppuntamenti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
