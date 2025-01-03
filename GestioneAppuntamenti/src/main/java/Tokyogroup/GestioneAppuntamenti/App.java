f744b51c900c1d08fc8bf7d8c85c91f8bcfd1020
a-roggeri <andrearoggeri22@gmail.com>
2025-01-03 16:11:16 +0100

    Creazione progetto

---
+++ GestioneAppuntamenti/src/main/java/Tokyogroup/GestioneAppuntamenti/App.java
@@ -0,0 +1,10 @@
+package Tokyogroup.GestioneAppuntamenti;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
