7212567eb6bfbc68c8564c34d3787770c41b8bed
a-roggeri <andrearoggeri22@gmail.com>
2025-01-26 01:33:24 +0100

    Analisi con CodeMR

---
+++ GestioneAppuntamenti/codemr/GestioneAppuntamenti/html/html_report_gestioneappuntamenti/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,14 @@
+var matrix = [[0,11,1,0],[0,0,0,0],[15,9,0,0],[0,1,0,0]]
+var packages = [{
+"name": " Tokyogroup.GestioneAppuntamenti.controller", "color": " #3182bd"
+}
+,{
+"name": " Tokyogroup.GestioneAppuntamenti.model", "color": " #6baed6"
+}
+,{
+"name": " Tokyogroup.GestioneAppuntamenti.view", "color": " #9ecae1"
+}
+,{
+"name": " Tokyogroup.GestioneAppuntamenti", "color": " #c6dbef"
+}
+];
---
+++ GestioneAppuntamenti/codemr/GestioneAppuntamenti/html/main_report/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"GestioneAppuntamenti", "Number of Highly Problematic Classes":0, "Number of Entities":36, "Number of Problematic Classes":0, "Class Lines of Code":2816, "Number of External Packages":19, "Number of Packages":4, "Number of External Entities":116}
